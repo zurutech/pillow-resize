aa4a017af0141817bf90d18cc101d50cf7696b45
Marco Venturelli <marco@zuru.com>
2023-03-21 14:56:03 +0100

    ✏️ fix dates

--- tests/main.cc
+++ tests/main.cc
@@ -1,5 +1,5 @@
 /*
-* Copyright 2022 Zuru Tech HK Limited
+* Copyright 2023 Zuru Tech HK Limited
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
